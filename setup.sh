4207a8f5ec70dd4bcb2c4bd3120abdb8c6b99397
hat3ph <88069788+hat3ph@users.noreply.github.com>
2024-03-15 15:14:18 +0800

    install firefox-esr for debian12

--- setup.sh
+++ setup.sh
@@ -26,15 +26,19 @@ install () {
    	# install firefox without snap
     # https://www.omgubuntu.co.uk/2022/04/how-to-install-firefox-deb-apt-ubuntu-22-04
     if [[ $firefox_deb == "yes" ]]; then
-		sudo install -d -m 0755 /etc/apt/keyrings
-		wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
-		echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
-		echo '
-		Package: *
-		Pin: origin packages.mozilla.org
-		Pin-Priority: 1000
-		' | sudo tee /etc/apt/preferences.d/mozilla
-		sudo apt update && sudo apt install firefox
+		if [[ -n "$(uname -a | grep Ubuntu)" ]]; then
+			sudo install -d -m 0755 /etc/apt/keyrings
+			wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+			echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+			echo '
+			Package: *
+			Pin: origin packages.mozilla.org
+			Pin-Priority: 1000
+			' | sudo tee /etc/apt/preferences.d/mozilla
+			sudo apt-get update && sudo apt-get install firefox
+		else
+			sudo apt-get install firefox-esr
+		fi
     fi
 
 	# copy my icewm configuration
